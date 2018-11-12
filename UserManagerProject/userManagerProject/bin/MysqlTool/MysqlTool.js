3256b5a1c2485ed42157c8acd9c1504c86c5cbbb
nhljh1234 <1015165558@qq.com>
2018-11-12 23:49:33 +0800

    修改数据库地址

--- UserManagerProject/userManagerProject/bin/MysqlTool/MysqlTool.js
+++ UserManagerProject/userManagerProject/bin/MysqlTool/MysqlTool.js
@@ -17,12 +17,13 @@ local.mysqlObjSave = {};
 
 //host名
 //const HOST = "localhost";
-const HOST = "localhost";
+const HOST = "rm-8vbo59n8wpuz4o7wg9o.mysql.zhangbei.rds.aliyuncs.com";
 //用户名
 const USER_NAME = "root";
 //密码
 //const PASSWORD = "root";
-const PASSWORD = "xiaoliao";
+//const PASSWORD = "xiaoliao";
+const PASSWORD = "Xiaoliao12341234";
 
 /**
  * 根据一个table表的名字新建一个链接对象
