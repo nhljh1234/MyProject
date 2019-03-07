37f092122369742714f61eb809d48329c6534bce
nhljh1234 <1015165558@qq.com>
2019-03-07 17:51:30 +0800

    ScrollView 效率优化

--- Unity/Material Merge/Assets/Script/SceneScript/ScrollView.cs
+++ Unity/Material Merge/Assets/Script/SceneScript/ScrollView.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class ScrollView : MonoBehaviour
 {
 
-    public GameObject button;
-    public GameObject content;
+    public GameObject scrollview;
 
     // Use this for initialization
     void Start()
     {
         int buttonSize = 1000;
+        /**
         //修改第一个
         TJ_UNITY_TOOL.GameObjectTool.findChild(button.transform, "Text").GetComponent<Text>().text = "0";
         for (int i = 0; i < buttonSize - 1; i++)
@@ -26,6 +26,18 @@ public class ScrollView : MonoBehaviour
         //重新计算位置
         RectTransform rectTransform = content.GetComponent<RectTransform>();
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, buttonSize * content.GetComponent<GridLayoutGroup>().cellSize.y);
+        **/
+        UGUI_TOOL.ScrollViewTool scrollviewTool = scrollview.GetComponent<UGUI_TOOL.ScrollViewTool>();
+        scrollviewTool.initData((item, index) =>
+        {
+            Transform transform = TJ_UNITY_TOOL.GameObjectTool.findChild(item, "Text");
+            item.name = "" + index;
+            transform.GetComponent<Text>().text = "" + index;
+        });
+        if (scrollviewTool != null)
+        {
+            scrollviewTool.startShow(buttonSize);
+        }
     }
 
     // Update is called once per frame
--- Unity/Material Merge/Assets/Script/Tool/UITool/ScrollViewTool.cs
+++ Unity/Material Merge/Assets/Script/Tool/UITool/ScrollViewTool.cs
@@ -1,95 +1,221 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class ScrollViewTool : MonoBehaviour
+namespace UGUI_TOOL
 {
-
-    //滑动列表节点
-    private GameObject _scrollview;
-    //克隆节点
-    private GameObject _tmp;
-    //content节点
-    private GameObject _content;
-    //滑动方式
-    private int _scrollMode;
-    //节点间距
-    private int _offset = 0;
-    //节点高度/宽度
-    private int _itemSize = 0;
-
-    //存储的节点，这边就是用于显示的节点
-    private GameObject[] gameObjects;
-
-    //水平
-    public static int HORIZONTAL = 1;
-    //垂直
-    public static int VERTICAL = 2;
-
-    public delegate void UpdateListItemEvent(Transform item, int index);
-    private UpdateListItemEvent _updateItem = null;
-
-    public void initData(GameObject scrollview, GameObject tmp, GameObject content, int scrollMode,
-        int offsetNum, int itemSize, UpdateListItemEvent updateItem)
+    public class ScrollViewTool : MonoBehaviour
     {
-        _scrollview = scrollview;
-        _tmp = tmp;
-        _content = content;
-        _scrollMode = scrollMode == HORIZONTAL ? HORIZONTAL : VERTICAL;
-        _offset = offsetNum;
-        _itemSize = itemSize;
-        _updateItem = updateItem;
-    }
-
-    //初始化显示
-    public void startShow(int showItemNum)
-    {
-
-    }
-
-    //刷新显示
-    public void refresh(int showItemNum)
-    {
-
-    }
-
-    private void showList(int showItemNum)
-    {
-        int showNum = (int)Mathf.Min(mathItemNum(), showItemNum);
-        buildShowArray(showNum);
-        //
-    }
-
-    //初始化用于显示的节点数组
-    private void buildShowArray(int showNum)
-    {
-        gameObjects = new GameObject[showNum];
-        //隐藏不用的节点
-        for (int i = showNum - 1; i < _content.transform.childCount; i++)
+        private class TransformData
         {
-            _content.transform.GetChild(i).gameObject.SetActive(false);
+            public Transform transform;
+            public int index = -1;
         }
-        //看看tmp的父节点上面有没有足够的节点
-        for (int i = 0; i < showNum && i < _content.transform.childCount; i++)
+        //滑动列表节点
+        public Transform scrollview;
+        //content节点
+        public Transform content;
+        //克隆节点
+        public Transform tmp;
+        //滑动方式
+        public ScrollType scrollType = ScrollType.VERTICAL;
+        //节点间距
+        public int offset = 0;
+        //节点高度/宽度
+        public int itemSize = 0;
+        //初始化的位置
+        public Vector2 initPos;
+
+        //存储的节点，这边就是用于显示的节点
+        private TransformData[] transformDatas;
+
+        //水平
+        public static int HORIZONTAL = 1;
+        //垂直
+        public static int VERTICAL = 2;
+
+        public enum ScrollType
         {
-            gameObjects[i] = _content.transform.GetChild(i).gameObject;
-            gameObjects[i].SetActive(true);
+            //水平
+            HORIZONTAL = 1,
+            //竖直
+            VERTICAL = 2,
         }
-        //不够的话就新建
-        for (int i = _content.transform.childCount; i < showNum; i++)
+        public delegate void UpdateListItemEvent(Transform item, int index);
+        private UpdateListItemEvent _updateItem = null;
+
+        public void initData(UpdateListItemEvent updateItem)
         {
-            GameObject newObject = Instantiate(_tmp);
-            //绑定节点
-            newObject.transform.SetParent(_content.transform);
-            //放进显示的列表
-            gameObjects[i] = newObject;
-            gameObjects[i].SetActive(true);
+            _updateItem = updateItem;
+        }
+
+        //初始化显示
+        public void startShow(int showItemNum)
+        {
+            showList(showItemNum);
+            initItemPos();
+            //保证只监听一次
+            scrollview.GetComponent<ScrollRect>().onValueChanged.RemoveListener(OnValueChange);
+            scrollview.GetComponent<ScrollRect>().onValueChanged.AddListener(OnValueChange);
+        }
+
+        //刷新显示
+        public void refresh(int showItemNum)
+        {
+            showList(showItemNum);
+        }
+
+        //监听值变化
+        private void OnValueChange(Vector2 pos)
+        {
+            //计算需要显示的最小的index
+            int minIndex = getMinIndex();
+            //获取所有超出屏幕的节点
+            TransformData[] changePosTransforms = getOverViewTransformDatas(minIndex);
+            if (changePosTransforms.Length == 0)
+            {
+                return;
+            }
+            int count = 0;
+            for (int i = minIndex; i < minIndex + transformDatas.Length; i++)
+            {
+                if (!judgeHaveIndex(i))
+                {
+                    if (count >= changePosTransforms.Length)
+                    {
+                        print("error! changePosTransforms less than need!");
+                        continue;
+                    }
+                    updateItemData(changePosTransforms[count], i);
+                    count++;
+                }
+            }
+            //_updateItem(changePosTransform, newIndex);
+        }
+
+        private void updateItemData(TransformData transformData, int index)
+        {
+            if (scrollType == ScrollType.HORIZONTAL)
+            {
+                transformData.transform.localPosition = new Vector2(initPos.x + (itemSize + offset) * index, initPos.y);
+                transformData.index = index;
+                _updateItem(transformData.transform, index);
+            }
+            else
+            {
+                transformData.transform.localPosition = new Vector2(initPos.x, initPos.y - (itemSize + offset) * index);
+                transformData.index = index;
+                _updateItem(transformData.transform, index);
+            }
+        }
+
+        private bool judgeHaveIndex(int index)
+        {
+            for (int i = 0; i < transformDatas.Length; i++)
+            {
+                if (transformDatas[i].index == index)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int getMinIndex()
+        {
+            if (scrollType == ScrollType.HORIZONTAL)
+            {
+                return Mathf.FloorToInt(Mathf.Abs(content.localPosition.x) / (itemSize + offset));
+            }
+            else
+            {
+                return Mathf.FloorToInt(Mathf.Abs(content.localPosition.y) / (itemSize + offset));
+            }
+        }
+        //获取所有超出屏幕的节点
+        private TransformData[] getOverViewTransformDatas(int minIndex)
+        {
+            List<TransformData> changePosTransforms = new List<TransformData>();
+            for (int i = 0; i < transformDatas.Length; i++)
+            {
+                if (transformDatas[i].index < minIndex || transformDatas[i].index >= (minIndex + transformDatas.Length))
+                {
+                    changePosTransforms.Add(transformDatas[i]);
+                }
+            }
+            return changePosTransforms.ToArray();
+        }
+
+        //初始化位置
+        private void initItemPos()
+        {
+            for (int i = 0; i < transformDatas.Length; i++)
+            {
+                updateItemData(transformDatas[i], i);
+            }
+        }
+
+        //显示列表
+        private void showList(int showItemNum)
+        {
+            int showNum = (int)Mathf.Min(mathItemNum(), showItemNum);
+            buildShowArray(showNum);
+            updateContentSize(showItemNum);
+        }
+
+        //更新content节点的尺寸
+        private void updateContentSize(int showItemNum)
+        {
+            int newSize = showItemNum * itemSize + (showItemNum - 1) * offset;
+            RectTransform rectTransform = content.GetComponent<RectTransform>();
+            if (scrollType == ScrollType.HORIZONTAL)
+            {
+                rectTransform.sizeDelta = new Vector2(newSize, rectTransform.sizeDelta.y);
+            }
+            else
+            {
+                rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, newSize);
+            }
+        }
+
+        //初始化用于显示的节点数组
+        private void buildShowArray(int showNum)
+        {
+            transformDatas = new TransformData[showNum];
+            for (int i = 0; i < transformDatas.Length; i++)
+            {
+                transformDatas[i] = new TransformData();
+            }
+            //隐藏不用的节点
+            for (int i = showNum - 1; i < content.childCount; i++)
+            {
+                content.GetChild(i).gameObject.SetActive(false);
+            }
+            //看看tmp的父节点上面有没有足够的节点
+            for (int i = 0; i < showNum && i < content.childCount; i++)
+            {
+                transformDatas[i].transform = content.GetChild(i);
+                transformDatas[i].transform.gameObject.SetActive(true);
+                transformDatas[i].transform.localPosition = initPos;
+            }
+            //不够的话就新建
+            for (int i = content.transform.childCount; i < showNum; i++)
+            {
+                Transform newObject = Instantiate(tmp);
+                //绑定节点
+                newObject.SetParent(content);
+                //放进显示的列表
+                transformDatas[i].transform = newObject;
+                transformDatas[i].transform.gameObject.SetActive(true);
+                transformDatas[i].transform.localPosition = initPos;
+            }
+        }
+
+        private int mathItemNum()
+        {
+            float maskHeigth = scrollview.GetComponent<RectTransform>().sizeDelta.y;
+            return ((int)Mathf.Ceil(maskHeigth / (itemSize + offset))) + 1;
         }
     }
 
-    private int mathItemNum()
-    {
-        float maskHeigth = _scrollview.GetComponent<RectTransform>().sizeDelta.y;
-        return ((int)Mathf.Ceil(maskHeigth / (_itemSize + _offset))) + 1;
-    }
 }
