4c1d2e759d2f5d06da08970119345d5176bcab19
nhljh1234 <1015165558@qq.com>
2019-03-23 22:10:30 +0800

    Animation 学习

---
+++ Unity/Material Merge/Assets/Script/SceneScript/Frame_Animation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Frame_Animation : MonoBehaviour
+{
+
+    public GameObject sprite;
+    public Button button_1;
+    public Button button_2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Animator animator = sprite.GetComponent<Animator>();
+        button_1.onClick.AddListener(() =>
+        {
+            animator.SetInteger("ChangeStatus", 1);
+        });
+        button_2.onClick.AddListener(() =>
+		{
+			animator.SetInteger("ChangeStatus", 2);
+		});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity/Material Merge/Assets/Script/SceneScript/Frame_Animation_Event.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frame_Animation_Event : MonoBehaviour
+{
+	public void showMsg(string msg, string msg_1)
+	{
+		print(msg);
+	}
+}
