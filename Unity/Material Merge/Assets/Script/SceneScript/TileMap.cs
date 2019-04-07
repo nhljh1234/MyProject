49e85a3a0963b14b519e21ba82d5d8a6d8814908
nhljh1234 <1015165558@qq.com>
2019-04-07 22:41:03 +0800

    Unity TileMap 学习

---
+++ Unity/Material Merge/Assets/Script/SceneScript/TileMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMap : MonoBehaviour
+{
+    public Tilemap tileMap;
+    public TileBase tile;
+
+    void Start()
+    {
+        
+    }
+}
