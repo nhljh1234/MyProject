d3fad65dcf593cd1350ad7dec75bedaa39e93a79
nhljh1234 <1015165558@qq.com>
2019-04-30 17:20:20 +0800

    Unity 赛车研究

--- Unity/Material Merge/Assets/Script/SceneScript/ShaderLearn.cs
+++ Unity/Material Merge/Assets/Script/SceneScript/ShaderLearn.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shader : MonoBehaviour
+public class ShaderLearn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
