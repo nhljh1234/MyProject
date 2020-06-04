ec680c01c9cb6cc60a8e2b537dd329997f9e9ea0
nhljh1234 <1015165558@qq.com>
2020-06-04 18:02:00 +0800

    1.代码整理 2.新增SRP教程

---
+++ Unity/SRP/Assets/Script/Editor/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering;
+
+public class MenuScript
+{
+    [MenuItem("SRP/CreateSPR_1")]
+    public static void CreateSPR_1()
+    {
+        var instance = ScriptableObject.CreateInstance<SRPAsset_1>();
+        AssetDatabase.CreateAsset(instance, "Assets/SRP/SPR_1.asset");
+        GraphicsSettings.renderPipelineAsset = instance;
+    }
+
+    [MenuItem("SRP/CreateSPR_2")]
+    public static void CreateSPR_2()
+    {
+        var instance = ScriptableObject.CreateInstance<SRPAsset_2>();
+        AssetDatabase.CreateAsset(instance, "Assets/SRP/SPR_2.asset");
+        GraphicsSettings.renderPipelineAsset = instance;
+    }
+}
---
+++ Unity/SRP/Assets/Script/SRP_1/SRPAsset_1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[ExecuteInEditMode]
+public class SRPAsset_1 : RenderPipelineAsset
+{
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new SRP_1();
+    }
+}
